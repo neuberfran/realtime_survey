999424a0f1c73f525d28b2736ade3dce67075f9b
neuberfran <neuberfran@gmail.com>
2021-04-15 20:23:54 -0300

    third commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,33 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"     // mexi nessas 4 e ..........     
+// import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import "../jquery/jquery-1.6.1.min"
+import "../jquery/jquery.charts"
+import "../jquery/jquery.notice"
+
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("chartkick/chart.js")
+//require("jquery")
+
+// import "bootstrap"
+// import "../stylesheets/application"
+
+var jQuery = require('jquery')
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()                  // Mexi nessas 3
--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,6 @@ RealtimeSurvey::Application.initialize!
 
 require 'pusher'
 
-Pusher.app_id = '999999'
-Pusher.key = '999999999999999999999'
-Pusher.secret = '99999999999999999'
+Pusher.app_id = '58892'
+Pusher.key = '3277c48ffd35616fee78'
+Pusher.secret = 'af028a9f25039763818c'
